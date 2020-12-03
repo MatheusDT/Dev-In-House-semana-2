cdaaa7b4fd5cb2c831386652a68c2d3dab7046a4
Matheus Terencio <matheus.sfg@hotmail.com>
2020-12-02 23:28:34 -0300

    correção de bug na criação de um item, e adicionado css

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var ul = document.getElementById("myList"); // a variavel declarada ul está recebendo o valor da tag que possue myList como identificador
 var li;
 var itemId;
 var item;
 addTask= function (){
-   //linha 48 está fazendo uma verificação do valor do documento e diferente de vazio.!!!      
+    var ul = document.getElementById("myList"); // a variavel declarada ul está recebendo o valor da tag que possue myList como identificador
+    console.log(ul)
+    //linha 48 está fazendo uma verificação do valor do documento e diferente de vazio.!!!      
    if (document.getElementById("task").value != ""){
-       item = document.getElementById("task").value;    // = recebe um valor do input task
-       itemId= ul.childElementCount;        //está recebendo a propriedade childelementcount da lista,que corresponde ao numero de elementos que está lista contém
-       li = createItemEl(item.value,itemId); // li esta recebendo um elemento de lista criado pela createItemEl
+       var item = document.getElementById("task").value;    // = recebe um valor do input task
+       var itemId= ul.childElementCount;        //está recebendo a propriedade childelementcount da lista,que corresponde ao numero de elementos que está lista contém
+       var li = createItemEl(item.value,itemId); // li esta recebendo um elemento de lista criado pela createItemEl
        li.appendChild(createRemoveTaskBtn(itemId)); 
        ul.appendChild(li); // ul está recebendo um item li
    }
